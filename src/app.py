df52251d1d428161d872412b8723a1b560e0bd5f
ovciarik <martin.ovciarik@gmail.com>
2018-09-10 20:30:11 +0100

    automatic theme generation, bug fixes

--- src/app.py
+++ src/app.py
@@ -10,16 +10,19 @@ from markdown import markdown
 
 # TODO load .md file and render html template [done]
 # TODO generate index from folders and files [done]
+# TODO create basic template: sidebar + main window [done]
+#     TODO create template from index.html [done]
+# TODO center that fucking css [done]
+# TODO create solarized dark css theme [done]
+# TODO create acme theme [done]
+# TODO create hackernews theme [done]
+# TODO create theme switcher [done]
+# TODO automaticaly generate themes [done]
 
-# TODO create basic template: sidebar + main window []
-#     TODO create template from index.html
-# TODO center that fucking css
-
-# TODO create solarized dark css theme []
-# TODO create acme theme []
-# TODO create hackernews theme []
-# TODO create theme switcher []
-
+# TODO block quote theme bug []
+# TODO save theme as cookie []
+# TODO expandable file tree view []
+# TODO sidebar toggle as arrow in left middle of screen []
 
 # domain ovciarik.io
 # alternative between github and common markdown
@@ -36,29 +39,35 @@ def common_md_to_html(f):
 # list all folders and files
 # generate index
 
-
-
 @app.route('/favicon.ico')
 def send_favicon():
     return send_from_directory('./static/ico/', 'favicon.ico')
 
+def render_page(content):
+    theme_name = request.args.get('theme', 'default')
+    theme_location = 'css/theme_{}.css'.format(theme_name)
+
+    sidebar_status = request.args.get('sidebar', 'on')
+
+    if sidebar_status == 'off':
+        sidebar_file = 'css/style_sidebar_off.css'
+    else:
+        sidebar_file = '' 
+
+    return render_template(
+        'index.html', 
+        theme_file=theme_location, 
+        sidebar=get_index('./blog', theme_name, sidebar_status), 
+        content=content, 
+        themes=get_themes(None),
+        theme_name=theme_name,
+        sidebar_file=sidebar_file,
+        sidebar_status=sidebar_status)
 
 @app.route('/')
 def test():
-    template = 'index_default.html'
-    theme = 'default'
-
-    try: 
-        theme = request.args["theme"]
-        if theme == 'dark':
-            template = 'index_dark.html'
-        if theme == 'light':
-            template = 'index_light.html'
-    except:
-        pass
-
-    return render_template(template, sidebar=get_index('./blog', theme), content='')
-
+    content = ''
+    return render_page(content)
 
 @app.route('/<path:path>')
 def get_md(path):
@@ -72,31 +81,37 @@ def get_md(path):
         return send_from_directory('./static/js', path.split('js/')[-1])
 
     file_path = './blog/{}'.format(path)
-
-    template = 'index_default.html'
-    theme = 'default'
-
-    try: 
-        theme = request.args["theme"]
-        if theme == 'dark':
-            template = 'index_dark.html'
-        if theme == 'light':
-            template = 'index_light.html'
-    except:
-        pass
-
-    return render_template(template, sidebar=get_index('./blog', theme), content=gget_md(file_path))
+    content = gget_md(file_path)
+    return render_page(content)
 
 
-def get_index(path, theme):
+def get_themes(path):
+    file_stuff = os.walk('./static/css')
+    # print('------------------------')
+    for xx in file_stuff:
+        # print(xx)
+        file_list = xx[2]
+        # print(file_list)
+        file_list = filter(lambda x: 'theme_' in x, file_list)
+        # print(file_list)
+        file_list = map(lambda x: x.replace('theme_', '').replace('.css', ''), file_list)
+        # print(file_list)
+        # print('------------------------')
+        file_list = sorted(file_list)
+        return file_list
+
+
+
+def get_index(path, theme, sidebar_status):
 
     file_list = os.walk('./blog')
 
-    index = '[/](/)\n\n'
+    sufix = '?theme={}&sidebar={}'.format(theme, sidebar_status)
+
+    index = '[/](/{})\n\n'.format(sufix)
 
     for xx in file_list:
         prefix = xx[0].replace('./blog', '')
-        sufix = '?theme=' + theme
         for xxx in xx[2]:
             element = prefix + '/' + xxx
             url = prefix + '/' + xxx + sufix
@@ -109,13 +124,9 @@ def gget_md(file_path):
         return common_md_to_html(f.read())
 
 
-def generate_index(list_of_folders):
-    # should return string Markdown index generated form list of folders
-    pass
 
-
-if __name__ == '__main__':
-    x = os.walk('./blog')
-    x = list(x)
-    print(x)
+# if __name__ == '__main__':
+    # x = os.walk('./blog')
+    # x = list(x)
+    # print(x)
     # print(f'{x}')
--- src/blog/stuff.md
+++ src/blog/stuff.md
@@ -1,1 +1,12 @@
 stuff
+=====
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+---
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
